fa9ed9b801317770bf3c44c20d4e1275c7024510
crL96 <190506813+crL96@users.noreply.github.com>
2025-02-14 16:21:28 +0100

    Add LinkedList class with mulitiple methods

---
+++ linkedList.js
@@ -0,0 +1,100 @@
+import Node from "./linkedListNode.js";
+
+class LinkedList {
+    constructor() {
+         this.head = null;
+    }
+
+    append(value) {
+        if (this.head == null) {
+            this.prepend(value);
+        }
+        let currentNode = this.head;
+        while (currentNode.nextNode != null) currentNode = currentNode.nextNode;
+        currentNode.nextNode = new Node(value);
+    }
+
+    prepend(value) {
+        const newNode = new Node(value, this.head);
+        this.head = newNode;
+    }
+
+    size() {
+        if (this.head == null) return 0;
+        let counter = 1;
+        let currentNode = this.head;
+        while (currentNode.nextNode != null) {
+            currentNode = currentNode.nextNode;
+            counter++;
+        }
+        return counter;
+    }
+
+    returnHead() {
+        return this.head;
+    }
+
+    tail() {
+        if (this.head == null) throw Error("List is empty");
+
+        let currentNode = this.head;
+        while (currentNode.nextNode != null) currentNode = currentNode.nextNode;
+        return currentNode;
+    }
+
+    at(index) {
+        if (this.head == null) throw Error("List is empty");
+
+        let currentNode = this.head;
+        for (let i = 0; i < index; i++) {
+            currentNode = currentNode.nextNode;
+            if (currentNode == null) return null;
+        }
+        return currentNode;
+    }
+
+    pop() {
+        if (this.head == null) throw Error("List is empty");
+
+        let currentNode = this.head;
+        while (currentNode.nextNode.nextNode != null) currentNode = currentNode.nextNode;
+        currentNode.nextNode = null;
+    }
+
+    contains(value) {
+        if (this.head == null) throw Error("List is empty");
+
+        let currentNode = this.head;
+        while (currentNode != null) {
+            if (currentNode.value == value) return true;
+            currentNode = currentNode.nextNode;
+        }
+        return false;
+    }
+
+    find(value) {
+        if (this.head == null) throw Error("List is empty");
+        let indexCounter = 0;
+        let currentNode = this.head;
+        while (currentNode != null) {
+            if (currentNode.value == value) return indexCounter;
+            currentNode = currentNode.nextNode;
+            indexCounter++;
+        }
+        return null;
+    }
+
+    toString() {
+        if (this.head == null) throw Error("List is empty");
+
+        let string = "";
+        let currentNode = this.head;
+        // console.log(currentNode.value);
+        while (currentNode != null) {
+            string = string.concat(`( ${currentNode.value} ) -> `);
+            currentNode = currentNode.nextNode;
+        }
+        string = string.concat("null");
+        return string;
+    }
+}
